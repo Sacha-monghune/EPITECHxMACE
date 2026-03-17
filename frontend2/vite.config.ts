cd6444b0a44556389b57098af7f63670cdcabc24
Iyad HOSSEN <93443451+ShyneOfficial@users.noreply.github.com>
2026-03-17 12:35:56 +0400

    ADD: Open website to network

--- frontend2/vite.config.ts
+++ frontend2/vite.config.ts
@@ -5,10 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/locations': 'http://localhost:3000',
       '/picture': 'http://localhost:3000',
       '/uploads': 'http://localhost:3000',
     },
+  },
+  preview: {
+    host: '0.0.0.0',
   },
 })
